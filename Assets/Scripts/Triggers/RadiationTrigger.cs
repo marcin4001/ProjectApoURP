fdf5a9f1f5f738ae584920447dc1573327907a9b
marcin4001 <61120035+marcin4001@users.noreply.github.com>
2024-08-30 11:25:59 +0200

    Added a trigger that adds radiation to the barrel

---
+++ Assets/Scripts/Triggers/RadiationTrigger.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using UnityEngine;
+
+public class RadiationTrigger : MonoBehaviour
+{
+    [SerializeField] private float timeAddRadiation = 2f;
+    private Coroutine coroutine;
+    private AudioSource source;
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            if(coroutine != null)
+            {
+                StopCoroutine(coroutine);
+            }
+            coroutine = StartCoroutine(AddRadiation());
+            source.Play();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if(coroutine != null) 
+            {
+                StopCoroutine(coroutine);
+                coroutine = null;
+            }
+            source.Stop();
+        }
+    }
+
+    private IEnumerator AddRadiation()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(timeAddRadiation);
+            PlayerStats.instance.AddOneRadLevel();
+        }
+    }
+}
