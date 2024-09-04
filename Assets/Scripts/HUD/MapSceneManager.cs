e76277d59f9e4fd80485a85b9be832f4c45bbc82
marcin4001 <61120035+marcin4001@users.noreply.github.com>
2024-09-04 23:30:36 +0200

    Added drawing of a dashed line between the player's position and the destination on the map

--- Assets/Scripts/HUD/MapSceneManager.cs
+++ Assets/Scripts/HUD/MapSceneManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,7 +11,13 @@ public class MapSceneManager : MonoBehaviour
     [SerializeField] private Button enterButton;
     [SerializeField] private Sprite enterBtnActiveSprite;
     [SerializeField] private Sprite enterBtnInactiveSprite;
+    [SerializeField] private RectTransform playerSign;
     [SerializeField] private RectTransform target;
+    [SerializeField] private Transform pathParent;
+    [SerializeField] private GameObject pathSegment;
+    [SerializeField] private float segmentLen = 10f;
+    [SerializeField] private float gapSegment = 5f;
+    private List<RectTransform> pathList = new List<RectTransform>();
     private Camera cam;
 
     private void Awake()
@@ -23,6 +30,7 @@ public class MapSceneManager : MonoBehaviour
         grid.SetActive(false);
         gridButton.onClick.AddListener(ShowGrid);
         enterButton.onClick.AddListener(OnClickEnter);
+        target.gameObject.SetActive(false);
     }
 
     private void ShowGrid()
@@ -47,6 +55,33 @@ public class MapSceneManager : MonoBehaviour
     {
         Vector2 newPoint;
         RectTransformUtility.ScreenPointToLocalPointInRectangle(map, Input.mousePosition, cam, out newPoint);
+        target.gameObject.SetActive(true);
         target.anchoredPosition = newPoint;
+        DrawPath();
+    }
+
+    private void DrawPath()
+    {
+        foreach(RectTransform path in pathList)
+        {
+            Destroy(path.gameObject);
+        }
+        pathList.Clear();
+        Vector2 playerPos = playerSign.anchoredPosition;
+        Vector2 targetPos = target.anchoredPosition;
+        float distance = Vector2.Distance(playerPos, targetPos);
+        Vector2 directionPath = (playerPos - targetPos).normalized;
+        int countSegment = Mathf.RoundToInt(distance / (segmentLen + gapSegment));
+        for(int i = 0; i < countSegment; i++)
+        {
+            RectTransform newSegment = Instantiate(pathSegment, pathParent).GetComponent<RectTransform>();
+            newSegment.name = $"Segment {i + 1}";
+            pathList.Add(newSegment);
+            newSegment.pivot = new Vector2(0f, 0.5f);
+            newSegment.anchoredPosition = targetPos + directionPath * (i * (segmentLen + gapSegment));
+            newSegment.sizeDelta = new Vector2(segmentLen, 2f);
+            float angleSegment = Mathf.Atan2(directionPath.y, directionPath.x) * Mathf.Rad2Deg;
+            newSegment.rotation = Quaternion.Euler(0f, 0f, angleSegment);
+        }
     }
 }
