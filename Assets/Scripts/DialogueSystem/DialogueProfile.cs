4630e1479e4309e3bc0c8d9cb78ffe3346b2f679
marcin4001 <61120035+marcin4001@users.noreply.github.com>
2024-10-16 22:13:12 +0200

    Changing Patrycja's initial dialogues

--- Assets/Scripts/DialogueSystem/DialogueProfile.cs
+++ Assets/Scripts/DialogueSystem/DialogueProfile.cs
@@ -6,6 +6,9 @@ public class DialogueProfile : ScriptableObject
     public string npcName;
     public string job;
     public string location;
+    [TextArea(3, 5)]
     public string firstReply;
+    [TextArea(3, 5)]
+    public string secReply;
     public DialogueNode[] dialogueNodes;
 }
