1acf8c536de9e01099edc6bc9d30b3c91d8466fb
marcin4001 <61120035+marcin4001@users.noreply.github.com>
2024-10-21 22:45:30 +0200

    Dialogues for Henry were added.

--- Assets/Scripts/InteractObj/ObjectInfoLog.cs
+++ Assets/Scripts/InteractObj/ObjectInfoLog.cs
@@ -4,9 +4,12 @@ using System.Linq;
 public class ObjectInfoLog : MonoBehaviour
 {
     [SerializeField] private string nameObject;
+    [SerializeField] private bool isNPC;
 
     public string GetLog()
     {
+        if(isNPC)
+            return $"This is {nameObject}";
         return $"This is {GetArticle()} {nameObject}";
     }
 
