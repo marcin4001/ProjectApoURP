548f503a2072fc8a13f6ef90dd08facdfc33ddd4
marcin4001 <61120035+marcin4001@users.noreply.github.com>
2025-03-11 13:41:08 +0100

    Added dialogues for Travis

--- Assets/Scripts/World/SunController.cs
+++ Assets/Scripts/World/SunController.cs
@@ -13,7 +13,7 @@ public class SunController : MonoBehaviour
         gameTime = FindFirstObjectByType<TimeGame>();
         sunTransform = sunLight.transform;
         playerLight = GameObject.FindGameObjectWithTag("PlayerLight").GetComponent<Light>();
-        playerLight.intensity = 0f;
+        //playerLight.intensity = 0f;
     }
 
 
@@ -30,22 +30,22 @@ public class SunController : MonoBehaviour
 
         if(timeGame >= sunriseTime && timeGame <= sunriseTime + 1f) 
         {
-            //playerLight.intensity = Mathf.Lerp(3f, 0f, (timeGame - sunriseTime) / 1f);
+            playerLight.intensity = Mathf.Lerp(3f, 0f, (timeGame - sunriseTime) / 1f);
             sunLight.intensity = Mathf.Lerp(0f, 1f, (timeGame - sunriseTime) / 1f);
         }
         else if(timeGame >= sunsetTime - 1f && timeGame <= sunsetTime)
         {
-            //playerLight.intensity = Mathf.Lerp(0f, 3f, (timeGame - (sunsetTime -1f)) / 1f);
+            playerLight.intensity = Mathf.Lerp(0f, 3f, (timeGame - (sunsetTime -1f)) / 1f);
             sunLight.intensity = Mathf.Lerp(1f, 0f, (timeGame - (sunsetTime - 1f)) / 1f);
         }
         else if(timeGame >= sunsetTime || timeGame < sunriseTime)
         {
-            //playerLight.intensity = 3f;
+            playerLight.intensity = 3f;
             sunLight.intensity = 0f;
         }
         else
         {
-            //playerLight.intensity = 0f;
+            playerLight.intensity = 0f;
             sunLight.intensity = 1f;
         }
         
