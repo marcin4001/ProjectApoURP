80ac26e480602ad21f28a50ac7624f4c45773883
marcin4001 <61120035+marcin4001@users.noreply.github.com>
2025-06-09 18:27:43 +0200

    A second quest for Nixon has been added

---
+++ Assets/Scripts/Test/TestQuest.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestQuest : MonoBehaviour
+{
+    [SerializeField] private List<ActionDialogue> actions = new List<ActionDialogue>();
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.F9))
+        {
+            foreach(ActionDialogue action in actions)
+            {
+                action.Execute();
+            }
+        }
+    }
+}
