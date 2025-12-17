3c9bad598afe4d69d3a49dedc8d024ad6f297ca3
marcin4001 <61120035+marcin4001@users.noreply.github.com>
2025-12-17 16:26:26 +0100

    A tooltip system has been added to every scene

--- Assets/Scripts/HUD/ToolTipObj.cs
+++ Assets/Scripts/HUD/ToolTipObj.cs
@@ -1,9 +1,12 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class ToolTipObj : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] private string tooltipText;
+    [SerializeField] private bool showAfterTime;
+    private Coroutine coroutine;
 
     public string GetText()
     {
@@ -17,11 +20,29 @@ public class ToolTipObj : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
 
     public void OnPointerEnter(PointerEventData eventData)
     {
+        if (showAfterTime)
+        {
+            if(coroutine != null)
+                StopCoroutine(coroutine);
+            coroutine = StartCoroutine(ShowAfterTime());
+            return;
+        }
         ToolTip.instance.SetText(tooltipText);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         ToolTip.instance.ExitHide();
+        if (showAfterTime)
+        {
+            if (coroutine != null)
+                StopCoroutine(coroutine);
+        }
+    }
+
+    private IEnumerator ShowAfterTime()
+    {
+        yield return new WaitForSeconds(1f);
+        ToolTip.instance.SetText(tooltipText);
     }
 }
