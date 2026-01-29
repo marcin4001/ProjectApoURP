294f7ab02fbc8ccbe1ddd9ac8b17eef7fc66b0cc
marcin4001 <61120035+marcin4001@users.noreply.github.com>
2026-01-29 15:14:41 +0100

    Added a door-opening mechanic in the bunker

---
+++ Assets/Scripts/InteractObj/MetalDoor.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using UnityEngine;
+
+public class MetalDoor : MonoBehaviour, IUsableObj
+{
+    [SerializeField] private Transform nearPoint;
+    [SerializeField] private Animator animatorDoor;
+    [SerializeField] private string openParam = "Open";
+    [SerializeField] private string openStartParam = "OpenStart";
+    [SerializeField] private string doorID;
+    private PlayerController playerController;
+    private bool isLock = true;
+
+    private void Start()
+    {
+        playerController = FindFirstObjectByType<PlayerController>();
+        if (GameParam.instance.DoorOnList(doorID))
+        {
+            animatorDoor.SetTrigger(openStartParam);
+            Destroy(this);
+        }
+    }
+
+    public bool CanUse()
+    {
+        return true;
+    }
+
+    public GameObject GetMainGameObject()
+    {
+        return gameObject;
+    }
+
+    public Vector3 GetNearPoint()
+    {
+        return nearPoint.position;
+    }
+
+    public void Use()
+    {
+        if(animatorDoor != null)
+            StartCoroutine(OpenDoor());
+    }
+
+    private IEnumerator OpenDoor()
+    {
+        playerController.SetBlock(true);
+        animatorDoor.SetTrigger(openParam);
+        yield return new WaitForSeconds(1);
+        playerController.SetBlock(false);
+        GameParam.instance.AddOpenDoor(doorID);
+        yield return new WaitForEndOfFrame();
+        Destroy(this);
+    }
+
+}
