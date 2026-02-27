c95c071cdf6b0f23af0a21775523131a90299c80
marcin4001 <61120035+marcin4001@users.noreply.github.com>
2026-02-27 13:17:24 +0100

    In the army camp, you can repair and start the car

---
+++ Assets/Scripts/InteractObj/MilitaryCar.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MilitaryCar : MonoBehaviour, IUsableObj
+{
+    [SerializeField] private SlotItem repairKit;
+    [SerializeField] private Transform nearPoint;
+    [SerializeField] private bool isRepair = false;
+    [SerializeField] private AudioClip repairClip;
+    [SerializeField] private AudioClip jeepClip;
+    [SerializeField] private int idPickUp;
+    [SerializeField] private string nextScene;
+    private AudioSource source;
+    private PlayerController playerController;
+
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+        playerController = FindFirstObjectByType<PlayerController>();
+        isRepair = PickUpObjList.instance.ExistOnList(idPickUp);
+    }
+    public bool CanUse()
+    {
+        return true;
+    }
+
+    public GameObject GetMainGameObject()
+    {
+        return gameObject;
+    }
+
+    public Vector3 GetNearPoint()
+    {
+       return nearPoint.position;
+    }
+
+    public void Use()
+    {
+        if(!isRepair)
+        {
+            bool result = Inventory.instance.PlayerHaveItem(repairKit.GetItem().id);
+            if (!result)
+            {
+                HUDController.instance.AddConsolelog("The car is broken.");
+                HUDController.instance.AddConsolelog("You need a repair kit.");
+                return;
+            }
+            Inventory.instance.RemoveItem(repairKit);
+            StartCoroutine(Repair());
+        }
+        else
+        {
+            StartCoroutine(StartEngine());
+        }
+    }
+
+
+    private IEnumerator Repair()
+    {
+        yield return new WaitForEndOfFrame();
+        playerController.SetBlock(true);
+        if (source != null)
+        {
+            source.clip = repairClip;
+            source.Play();
+        }
+        yield return new WaitForSeconds(1);
+        isRepair = true;
+        playerController.SetBlock(false);
+        HUDController.instance.AddConsolelog("The car has been repaired.");
+        PickUpObjList.instance.AddIdToList(idPickUp);
+    }
+
+    private IEnumerator StartEngine()
+    {
+        CameraMovement.instance.SetBlock(true);
+        FadeController.instance.SetFadeIn(true);
+        playerController.SetBlock(true);
+        yield return new WaitForSeconds(2f);
+        if (source != null)
+        {
+            source.spatialBlend = 0f;
+            source.clip = jeepClip;
+            source.Play();
+        }
+        yield return new WaitForSeconds(3.4f);
+        SceneManager.LoadScene(nextScene);
+    }
+}
