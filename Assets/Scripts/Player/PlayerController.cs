368cb9513701aa697de691038bc514e0bb08639f
marcin4001 <61120035+marcin4001@users.noreply.github.com>
2026-09-17 17:26:36 +0200

    Added wolves to the OuachitaNTScene

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -346,6 +346,11 @@ public class PlayerController : MonoBehaviour
                     return;
                 }
             }
+
+            if (!CombatController.instance.PlayerHaveAp(2))
+            {
+                return;
+            }
             animationPlayer.Punch();
             transform.rotation = Quaternion.LookRotation(point - transform.position);
             enemy.GetDamage(PlayerStats.instance.GetHandDamage(), true, false);
